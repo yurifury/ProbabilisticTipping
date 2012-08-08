06f49e6d32912134a8f0353c41f79888482aa0de
Yuri Feldman <yuri@tbqh.net>
2012-08-08 16:33:40 +1000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProbabilisticTipping::Application.config.secret_token = '28a6a22a320cccbbb94aaeaaa7759ef43187c84d839037a4f6f820d56b58fccb9032f9a6bf3a73463f31034cb123119918d982885007ec50dadd15412e61bed8'
