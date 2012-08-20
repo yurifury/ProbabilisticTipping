a3129e617cf4dcaa30ba3decd21229ce2041b7c5
Yuri Feldman <yuri@tbqh.net>
2012-08-20 13:28:26 +1000

    Alert dismissal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def bootstrap_alert_class(name)
+    debug name
+    case name
+    when :notice
+      "alert-success"
+    when :error
+      "alert-error"
+    else
+      "alert-info"
+    end
+  end
 end
