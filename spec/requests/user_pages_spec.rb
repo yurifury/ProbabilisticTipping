227100611d9f4ee84dbb71ffde3ced1c37365745
Yuri Feldman <yuri@tbqh.net>
2012-08-23 18:11:25 +1000

    Valid user creation test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
-describe "Static pages" do
-  describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/'
-      page.should have_content('Welcome')
+describe "User Pages" do
+  subject { page }
+  describe "signup" do
+    before { visit signup_path }
+
+    let(:submit) { "Sign up" }
+
+    describe "with invalid information" do
+      it "should should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Test User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "user_password_confirmation", with: "foobar"
+      end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count)
+      end
     end
   end
 end
