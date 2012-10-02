e2084ffc3422a4c67d03af41553b4cbc2e870cf1
Yuri Feldman <yuri@tbqh.net>
2012-10-02 19:30:25 +1000

    Creating matches with rounds

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,3 +1,31 @@
 class RoundsController < ApplicationController
-  before_filter :authorize, only: [:new, :create, :participate, :unparticipate]
+  before_filter :find_competition, only: [:new, :create, :show]
+  before_filter :check_owner, only: [:new, :create]
+
+  def new
+    @round = @competition.rounds.build
+  end
+
+  def create
+    @round = @competition.rounds.build(params[:round])
+    if @round.save
+      redirect_to competition_round_path(@competition, @round), notice: "Round created"
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @round = @competition.rounds.find(params[:id])
+  end
+
+  private
+    def find_competition
+      @competition = Competition.find(params[:competition_id])
+    end
+
+    def check_owner
+      @competition = Competition.find(params[:competition_id])
+      redirect_to root_path, alert: "You can't do that!" unless current_user?(@competition.owner)
+    end
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,7 +1,8 @@
 class Round < ActiveRecord::Base
-  attr_accessible :closing_time
+  attr_accessible :closing_time, :matches_attributes
   belongs_to :competition
   has_many :matches
+  accepts_nested_attributes_for :matches
 
   def open?
     Time.now < closing_time
